23d412e96a25da48f26e5aed98d872069954eb0e
alpheay <nik.nandi.1@gmail.com>
2023-04-21 18:09:14 -0400

    s

---
+++ HW11/Submission/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        // Create a new instance of the class
+        Test test = new Test();
+        // Call the method
+        test.run();
+    }
+
+    public void run() {
+        // Create a new instance of the class
+        Test test = new Test();
+        // Call the method
+        test.run();
+    }
+}
